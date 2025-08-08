48f494d3479adf156abc7d561bc9420231fe9d02
Silvio Heinze <mail@silvioheinze.eu>
2025-08-08 20:19:07 +0200

    Update ALLOWED_HOSTS in settings.py to include production domain. Added 'isrfield.dataplexity.eu' for deployment configuration.

--- app/isrfield/settings.py
+++ app/isrfield/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!7lxj&-vt5k^n@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'isrfield.dataplexity.eu']
 
 
 # Application definition
